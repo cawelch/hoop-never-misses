4ec6ba19cba22a4990ae83adb3d3f3f3b1109a7d
cawelch <70113022+cawelch@users.noreply.github.com>
2020-11-05 22:03:15 -0500

    Update test_points.py

--- test_points.py
+++ test_points.py
@@ -41,47 +41,42 @@ Parameters: phi - angle of the backboard with the horizontal
 
 Returns: boolean value indicating whether the ball is hitting the backboard
 """
-def hit_backboard(phi_array,y,z):
+def hit_backboard(phi_array,y,z,backboard_y,backboard_z):
     C = 0.7493 # circumference in m, from basketball's circumference of 29.5 inches
     radius = C/(2*np.pi)
     height_backboard = 1.0668
-    eps_backboard = 1e-2
+    eps_backboard = 1e-4
     eps = 1e-1
 
     theta = np.linspace(0,2*np.pi,100)
     circle_ys = y+radius*np.cos(theta)
     circle_zs = z+radius*np.sin(theta)
+    #print(circle_ys,circle_zs)
     #plt.plot(circle_ys,circle_zs)
-
-    for phi in phi_array:
-        slope = np.tan(phi)
+    #print(phi_array)
+    for j in range(len(phi_array)-1):
+        #print(len(phi_array)-1)
+        #print("j",j)
+        phi = phi_array[j]
+        #print(phi)
+        slope = np.tan(phi_array[j])
         for i in range(len(circle_ys)):
-            #plt.plot(circle_ys[i],circle_zs[i],'.')
-            if phi < np.pi/2:
-                if np.absolute(circle_ys[i]) >= eps_backboard and circle_ys[i] <= height_backboard*np.cos(phi):
-                    if circle_zs[i]-radius >= 3.048 and circle_zs[i] <= 3.048+height_backboard*np.sin(phi):
-                        #print('within rectangle')
-                        #print(np.absolute(circle_zs[i]-3.048-slope*circle_ys[i]))
-                        if np.absolute(circle_zs[i]-3.048-slope*circle_ys[i]) <= eps:
-                            #print('hit')
-                            hit_phi = phi
-                            return True,hit_phi
-            elif phi == np.pi/2:
-                if np.absolute(circle_ys[i]) <= eps_backboard:
-                    if circle_zs[i]-radius >= 3.048 and circle_zs[i] <= 3.048+height_backboard:
-                        #print('hit')
-                        hit_phi = phi
-                        return True,hit_phi
+            on_slope = np.absolute(circle_zs[i]-backboard_z[j]-slope*(circle_ys[i]-backboard_y[j]))
+            #print("here")
+            if phi <= np.pi/2:
+                if circle_ys[i] >= backboard_y[j] and circle_ys[i] <= backboard_y[j+1] and circle_zs[i] >= backboard_z[j] and circle_zs[i] <= backboard_z[j+1] and on_slope <= eps:
+                    hit_phi = phi
+                    #print("hit")
+                    return True, hit_phi
             else:
-                if circle_ys[i] >= height_backboard*np.cos(phi) and circle_ys[i] <= eps_backboard:
-                    if circle_zs[i]-radius >= 3.048 and circle_zs[i] <= 3.048+height_backboard*np.sin(phi):
-                        #print('within rectangle')
-                        if np.absolute(circle_zs[i]-3.048-slope*circle_ys[i]) <= eps:
-                            #print('hit')
-                            hit_phi = phi
-                            return True,hit_phi
+                if circle_ys[i] <= backboard_y[j] and circle_ys[i] >= backboard_y[j+1] and circle_zs[i] >= backboard_z[j] and circle_zs[i] <= backboard_z[j+1] and on_slope <= eps:
+                    hit_phi = phi
+                    #print("hit")
+                    return True, hit_phi
+            #print("i after",i)
 
     hit_phi = -1
+    #print("no")
     return False,hit_phi
 
 
@@ -140,7 +135,7 @@ Parameters: init - array of the initial y and z positions and velocities
             phi - angle that the backboard makes with the horizontal
 Returns: solved differential equations of the y and z positions and velocities
 """
-def RK4(init,phi_array):
+def RK4(init,phi_array,backboard_y,backboard_z):
     y_points = []
     z_points = []
     v_y_points = []
@@ -153,21 +148,21 @@ def RK4(init,phi_array):
 
     r = np.array(init, float)
     h = 0.01
-    #print(r[1],r[3],r[0],height_backboard*np.cos(phi)-0.1)
+    #print("hi")
     while (r[1] >= 3.048 or r[3] > 0) and r[0] >= min_y:
-        #print("y=",r[0])
-        #print("here")
+        #print("while")
+        #plt.plot(r[0],r[1])
         y_points.append(r[0])
         z_points.append(r[1])
         v_y_points.append(r[2])
         v_z_points.append(r[3])
 
-        backboard, hit_phi = hit_backboard(phi_array,r[0],r[1])
+        backboard, hit_phi = hit_backboard(phi_array,r[0],r[1],backboard_y,backboard_z)
+        #print(backboard)
         if (not stop) and backboard:
-            backboard = True
+            stop = True
             y_bounce, z_bounce, v_y_bounce, v_z_bounce = elastic_bounce(hit_phi,r[0],r[1],r[2],r[3])
             r = np.array([y_bounce,z_bounce,v_y_bounce,v_z_bounce],float)
-            #add in basket here
             if in_basket(r[0],r[1]):
                 basket = True
 
@@ -178,12 +173,11 @@ def RK4(init,phi_array):
             k4 = h*f(r+k3)
             r += (k1+2*k2+2*k3+k4)/6
 
-    """
-    if backboard:
+    if stop:
+        print("hit")
         return y_points, z_points, v_y_points, v_z_points,basket
     else:
         return [],[],[],[],basket
-    """
 
     return y_points,z_points,v_y_points,v_z_points,basket
 
@@ -209,31 +203,67 @@ def in_basket(y,z):
 
 
 def move_points(num_points):
-    backboard_y = [0]
-    backboard_z = [3.048]
+    backboard_y = np.zeros(num_points) #[0,-0.1,0]
+    backboard_z = np.zeros(num_points) #[3.048,3.5814,4.1148]
+    backboard_z[0] = 3.048
 
     for i in range(1,num_points):
-        backboard_z.append(backboard_z[i-1]+(4.1148-3.048)/(num_points-1))
-        backboard_y.append(np.random.uniform(-0.5,0.5))
-
+        backboard_z[i]=(backboard_z[i-1]+(4.1148-3.048)/(num_points-1))
+        backboard_y[i]=(np.random.uniform(-0.5,0.5))
+    print(backboard_y,backboard_z)
     plt.plot(backboard_y,backboard_z)
     return backboard_y,backboard_z
 
 
+def percent_in():
+    num_backboards = 1
+    num_backboard_points = 3
+    num_shots = 100
+    backboard_y = np.zeros((num_backboards,num_backboard_points))
+    backboard_z = np.zeros((num_backboards,num_backboard_points))
+    shots_in = np.zeros(num_backboards)
+
+    for i in range(num_backboards):
+            back_y, back_z = move_points(num_backboard_points)
+            backboard_y[i] = back_y
+            backboard_z[i] = back_z
+            #print(backboard_y,backboard_z)
+            #backboard_y[i],backboard_z[i] = move_points(num_backboard_points)
+            #plt.plot(backboard_y[i],backboard_z[i])
+            del_y = []
+            del_z = []
+            #print(backboard_y)
+            #Creates an array of the change in y and z for each point in the backboard
+            for m in range(len(backboard_y[i])-1):
+                del_y.append(backboard_y[i][m+1]-backboard_y[i][m])
+                del_z.append(backboard_z[i][m+1]-backboard_z[i][m])
+            phi_array = np.arctan(np.array(del_z)/np.array(del_y))
+            for j in range(len(phi_array)):
+                if phi_array[j] < 0:
+                    phi_array[j] += np.pi
+
+    for k in range(num_shots):
+        random_y = 7.5#np.random.uniform(0.5,7) #7.5
+        random_z = 1.8 #np.random.uniform(1.5,2) #1.8
+        random_v0 = 9.8 #np.random.uniform(6,10) #9.8
+        random_theta = np.random.uniform(np.pi/2,np.pi) #2*np.pi/3
+        init = [random_y,random_z,random_v0*np.cos(random_theta),random_v0*np.sin(random_theta)]
+
+        for i in range(num_backboards):
+            y_points,z_points,v_y_points,v_z_points,basket = RK4(init,phi_array,backboard_y[i],backboard_z[i])
+            plt.plot(y_points,z_points)
+
+            if basket:
+                shots_in[i] += 1
+
+    percent_shots_in = np.array(shots_in)/num_shots
+    print(percent_shots_in)
+
 
 def main():
-    backboard_y,backboard_z = move_points(3)
-    plt.plot(backboard_y,backboard_z)
+    percent_in()
 
-    random_y = 8    #np.random.uniform(smallest_y,7)
-    random_z = 1.8 #np.random.uniform(1.5,2)
-    random_v0 = 9.8 #np.random.uniform(6,10)
-    random_theta = 2*np.pi/3 #np.random.uniform(np.pi/2,np.pi)
-    init = [random_y,random_z,random_v0*np.cos(random_theta),random_v0*np.sin(random_theta)]
-    phi_array = np.arctan(np.array(backboard_z),np.array(backboard_y))
-    y_points,z_points,v_y_points,v_z_points,basket = RK4(init,phi_array)
-    plt.plot(y_points,z_points)
-
+    plt.savefig("plot-output.png")
     plt.show()
 
 if __name__ == "__main__":
